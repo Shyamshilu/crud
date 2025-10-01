4ab48b4ee40fa4b8b8afc575001be7bc2fb258a3
Shyamshilu <shyamshilu77@gmail.com>
2025-10-01 13:31:24 +0530

    Update studentValidator.js

--- studentValidator.js
+++ studentValidator.js
@@ -1,23 +1,11 @@
 const Joi= require("joi");
 exports.createValidator=Joi.object({
-    title:Joi.string().min(3).max(30).required(),
-    artist:Joi.string().min(3).max(50).required(),
-    category:Joi.string().min(3).max(50).required(),
-    album:Joi.string().min(3).max(50).required(),
-    url:Joi.string().required(),
-    photo:Joi.string().required(),
-    year:Joi.number().min(1900).max(new
-        Date().getFullYear()).required(),
+    title:Joi.string().min(3).max(30),
+  
 });
 exports.updateValidator=Joi.object({
-    title:Joi.string().min(3).max(30).optional(),
-    artist:Joi.string().min(3).max(50).optional(),
-    category:Joi.string().min(3).max(50).optional(),
-    album:Joi.string().min(3).max(50).optional(),
-    url:Joi.string().uri().optional(),
-    photo:Joi.string().uri().optional(),
-    year:Joi.number().min(1900).max(new
-        Date().getFullYear()).optional(),
+    title:Joi.string().min(3).max(30),
+   
 });
 exports.validate=(schema,data)=>{
     const{error,value}=schema.validate(data,{abortEarly:false});
@@ -32,3 +20,4 @@ exports.validate=(schema,data)=>{
   }  
   return{success:true,value};
 };
+
