9f985e12260b59a2b1aa01ee91c392fa35634372
Shyamshilu <shyamshilu77@gmail.com>
2025-10-01 13:21:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ mongoose.connect(
     "mongodb+srv://shyamshilu:shyam0976@cluster1.icbajkg.mongodb.net/?retryWrites=true&w=majority&appName=cluster1",
   
 );
+console.log("hello world");
 app.use("/song",studentRoute);
 app.listen(PORT,()=>{
     console.log("server is running :127.0.0.1"+PORT);
 });
+
 
 
